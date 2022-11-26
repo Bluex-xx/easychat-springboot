da6d1ac3e4361e7cd32dd975ee5c9a06cad50510
unknown <sunbrian@foxmail.com>
2022-11-26 16:19:29 +0800

    feat：complete easychat

---
+++ src/main/java/com/czy/echat/common/WebMvcConfig.java
@@ -0,0 +1,23 @@
+package com.czy.echat.common;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * 描述:
+ * Author： hxx
+ * Date： 2022/10/28 13:28
+ **/
+//设置跨域相关
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedHeaders("Content-Type","X-Requested-With","accept,Origin","Access-Control-Request-Method","Access-Control-Request-Headers","token")
+                .allowedMethods("*")
+                .allowedOrigins("*")
+                .allowCredentials(true);
+    }
+}
+
